adbbdc5f0f19ca1e899dc97a772ce68c2cdb4fb2
lican <a.a.soroka05@gmail.com>
2022-11-24 15:47:17 +0400

    added connection to oracle

--- main.cpp
+++ main.cpp
@@ -1,17 +1,27 @@
 #include "mainwindow.h"
 
 #include <QApplication>
-#include <QSqlDatabase>
+#include <QtSql>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QSqlDatabase db = QSqlDatabase::addDatabase("QODBC", "mydb");
-    db.setHostName("bigblue");
-    db.setDatabaseName("flightdb");
-    db.setUserName("acarlson");
-    db.setPassword("1uTbSbAs");
+    QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+    db.setDatabaseName("DRIVER={Oracle in XE};Server=LOCALHOST\\SQLEXPRESS");
+    db.setUserName("system");
+    db.setPassword("Tybctqcrfz55");
+    if (!db.open())
+    {
+        qDebug() << db.lastError().text();
+    }
+    else {
+        qDebug() << "Wow opened";
+    }
+
+    QSqlQuery query;
+    query.exec("insert into section values(1, 's5', 'd5')");
 
     MainWindow w;
     w.show();
