4390846c58c42c229b7833734577a3bfb3ef8fab
lican <a.a.soroka05@gmail.com>
2022-11-24 20:21:56 +0400

    trying to fix

--- main.cpp
+++ main.cpp
@@ -1,29 +1,47 @@
 #include "mainwindow.h"
-
 #include <QApplication>
 #include <QtSql>
 #include <QDebug>
+#include <QTableView>
+#include <QHeaderView>
+#include <QSqlTableModel>
+#include <QTextCodec>
+#include <windows.h>
 
 int main(int argc, char *argv[])
 {
+    SetConsoleOutputCP(65001);
     QApplication a(argc, argv);
 
+    MainWindow *w = new MainWindow();
+
     QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
-    db.setDatabaseName("DRIVER={Oracle in XE};Server=LOCALHOST\\SQLEXPRESS");
-    db.setUserName("system");
-    db.setPassword("Tybctqcrfz55");
+    QString connectionTemplate = "DRIVER={Oracle in XE};SERVER=localhost;DATABASE=dblabs;Uid=sys;Pwd=Tybctqcrfz55 as sysdba;Trusted_Connection=Yes;";
+    db.setDatabaseName(connectionTemplate);
+
     if (!db.open())
     {
-        qDebug() << db.lastError().text();
+        qDebug() << db.lastError();
     }
     else {
         qDebug() << "Wow opened";
     }
 
-    QSqlQuery query;
-    query.exec("insert into section values(1, 's5', 'd5')");
+    QSqlTableModel *model = new QSqlTableModel(w, db);
+    model->setTable("participants");
+    model->setEditStrategy(QSqlTableModel::OnManualSubmit);
+    model->select();
+    qDebug() << model->lastError();
 
-    MainWindow w;
-    w.show();
+    QTableView *table = w->findChild<QTableView*>("table_view");
+    table->setModel(model);
+    table->setSelectionBehavior(QAbstractItemView::SelectRows);
+    table->setSelectionMode(QAbstractItemView::SingleSelection);
+    table->resizeColumnsToContents();
+    table->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    table->show();
+
+    w->show();
+
     return a.exec();
 }
